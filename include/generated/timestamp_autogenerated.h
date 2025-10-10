4014c725a97ba15eb16c8b22b8f12a3040c6cff9
Thomas Kang <tarou999@gmail.com>
2025-10-10 16:29:20 -0700

    [BUILD] Increase flash partition for applications
    New features:
    
    Changes:
    
    - Increase sizes of application partitions on the SPI flash to
      accommodate larger applications as well as applications including
      manufacturing features.
    
    Fixes:
    
    Issues:

--- include/generated/timestamp_autogenerated.h
+++ include/generated/timestamp_autogenerated.h
@@ -1,5 +1,5 @@
-#define WISE_DATE "Oct 08 2025"
-#define WISE_TIME "18:44:29"
+#define WISE_DATE "Oct 10 2025"
+#define WISE_TIME "16:06:38"
 #define WISE_TZ "-0700"
-#define WISE_DMI_DATE "10/08/2025"
-#define WISE_BUILD_DATE 0x20251008
+#define WISE_DMI_DATE "10/10/2025"
+#define WISE_BUILD_DATE 0x20251010
