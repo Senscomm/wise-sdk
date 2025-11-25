5f2b85b07597c8f6c67a1b1d56ca1cea4c6b4fac
Thomas Kang <tarou999@gmail.com>
2025-11-24 17:39:04 -0800

    Update version

--- include/generated/timestamp_autogenerated.h
+++ include/generated/timestamp_autogenerated.h
@@ -1,5 +1,5 @@
 #define WISE_DATE "Nov 24 2025"
-#define WISE_TIME "14:28:00"
-#define WISE_TZ "+0800"
+#define WISE_TIME "17:32:02"
+#define WISE_TZ "-0800"
 #define WISE_DMI_DATE "11/24/2025"
 #define WISE_BUILD_DATE 0x20251124
