04c20b2821806513a91096e4b75a7897b8fc802a
Thomas Kang <tarou999@gmail.com>
2026-01-21 12:48:30 -0800

    Update for wise-sdk-1.3.3

--- include/generated/timestamp_autogenerated.h
+++ include/generated/timestamp_autogenerated.h
@@ -1,5 +1,5 @@
 #define WISE_DATE "Jan 21 2026"
-#define WISE_TIME "14:49:55"
-#define WISE_TZ "+0800"
+#define WISE_TIME "12:41:58"
+#define WISE_TZ "-0800"
 #define WISE_DMI_DATE "01/21/2026"
 #define WISE_BUILD_DATE 0x20260121
